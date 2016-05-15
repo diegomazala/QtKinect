fe85db4f9cf24de6139124507155adf661dacd25
Diego Mazala <diegomazala@gmail.com>
2016-05-15 14:32:33 -0300

    Option to load from file or folder

--- src/KinectLoadMain.cpp
+++ src/KinectLoadMain.cpp
@@ -1,52 +1,67 @@
 #include <QApplication>
-#include <QMessageBox>
 #include <QDir>
-#include <QDateTime>
 #include "QImageWidget.h"
 #include "QKinectFile.h"
 #include "QKinectGrabberFromFile.h"
 #include "QKinectIO.h"
 #include <iostream>
 
-
+// Usage: ./KinectLoadd.exe ../knt_frames/ 5
+// Usage: ./KinectLoadd.exe ../knt_frames/frame_33.knt
 int main(int argc, char **argv)
 {
-	QDateTime now = QDateTime::currentDateTime();
-	QString input_folder = ".";
+	QString input_path = QDir::currentPath();
+	int fps = 30;
 
 	if (argc > 1)
-		input_folder = argv[1];
+		input_path = argv[1];
 
-	int fps = 30;
+	
 	if (argc > 2)
 		fps = atoi(argv[2]);
+
+	QKinectGrabberFromFile* kinect_grabber = nullptr;
+	QKinectFrame* kinect_frame = nullptr;
 
 	QApplication app(argc, argv);
 
 	int width = 640;
 	int height = 480;
 
-	QKinectGrabberFromFile* kinect = new QKinectGrabberFromFile();
-	kinect->setFolder(input_folder);
-	kinect->setFramesPerSecond(fps);
-	kinect->start();
-
-
 	QImageWidget colorWidget;
 	colorWidget.setMinimumSize(640, 480);
 	colorWidget.move(0, 0);
 	colorWidget.show();
-	QApplication::connect(kinect, SIGNAL(colorImage(QImage)), &colorWidget, SLOT(setImage(QImage)));
 
 	QImageWidget depthWidget;
 	depthWidget.setMinimumSize(640, 480);
-	depthWidget.move(50, 50);
+	depthWidget.move(640, 0);
 	depthWidget.show();
-	QApplication::connect(kinect, SIGNAL(depthImage(QImage)), &depthWidget, SLOT(setImage(QImage)));
+
+	if (QFileInfo(input_path).isDir())
+	{
+		kinect_grabber = new QKinectGrabberFromFile();
+		kinect_grabber->setFolder(input_path);
+		kinect_grabber->setFramesPerSecond(fps);
+		kinect_grabber->start();
+
+		QApplication::connect(kinect_grabber, SIGNAL(colorImage(QImage)), &colorWidget, SLOT(setImage(QImage)));
+		QApplication::connect(kinect_grabber, SIGNAL(depthImage(QImage)), &depthWidget, SLOT(setImage(QImage)));
+		QApplication::connect(kinect_grabber, SIGNAL(fileLoaded(QString)), &colorWidget, SLOT(setWindowTitle(QString)));
+		QApplication::connect(kinect_grabber, SIGNAL(fileLoaded(QString)), &depthWidget, SLOT(setWindowTitle(QString)));
+	}
+	else
+	{
+		kinect_frame = new QKinectFrame(input_path);
+		colorWidget.setImage(kinect_frame->convertColorToImage());
+		depthWidget.setImage(kinect_frame->convertDepthToImage());
+	}
 
 
 	int app_exit = app.exec();
-	kinect->stop();
+	
+	if (kinect_grabber)
+		kinect_grabber->stop();
 
 	return app_exit;
 }
