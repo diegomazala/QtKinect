fe9bed31f64fbcc64d6d0aff32e91aa5c1845a01
AVELL\Diego <diegomazala@gmail.com>
2016-09-01 18:40:33 -0300

    Reviwed normal generation in cpu

--- src/NormalEstimate.cpp
+++ src/NormalEstimate.cpp
@@ -35,20 +35,19 @@ int main(int argc, char **argv)
 	std::vector<Eigen::Vector3f> colors;
 
 
-	const int depth_map_width = 512;
-	const int depth_map_height = 424;
-	const float fovy = 70.0f;
-	const float aspect_ratio = static_cast<float>(depth_map_width) / static_cast<float>(depth_map_height);
-	const float near_plane = 0.1f;
-	const float far_plane = 10240.0f;
+	
 
 	try
 	{
 		timer.start();
 		KinectFrame frame;
 		KinectFrame::load(input_filename, frame);
-	
 		timer.print_interval("Importing kinect frame    : ");
+
+		const float fovy = 70.0f;
+		const float aspect_ratio = static_cast<float>(frame.depth_width()) / static_cast<float>(frame.depth_height());
+		const float near_plane = 0.1f;
+		const float far_plane = 10240.0f;
 
 		timer.start();
 		
@@ -57,19 +56,28 @@ int main(int argc, char **argv)
 
 		int i = 0;
 
-		for (int x = 1; x < depth_map_width - 1; ++x)
+		for (int x = 0; x < frame.depth_width() - 1; ++x)
 		{
-			for (int y = 1; y < depth_map_height - 1; ++y)
+			for (int y = 0; y < frame.depth_height() - 1; ++y)
 			{
-				const float depth = frame.depth[y * depth_map_width + x];
-				const float depth_u1v = frame.depth[y * depth_map_width + x + 1];
-				const float depth_uv1 = frame.depth[(y + 1) * depth_map_width + x];
+				const float depth = frame.depth[y * frame.depth_width() + x];
+				const float depth_u1v = frame.depth[y * frame.depth_width() + x + 1];
+				const float depth_uv1 = frame.depth[(y + 1) * frame.depth_width() + x];
 
 				if (depth < 0.01 || depth_u1v < 0.01 || depth_uv1 < 0.01)
 				{
 					continue; 
 
-					vert_uv = window_coord_to_3d(Eigen::Vector2f(x, y), depth, fovy, aspect_ratio, near_plane, far_plane, depth_map_width, depth_map_height);
+					vert_uv = window_coord_to_3d(
+						Eigen::Vector2f(x, y), 
+						depth, 
+						fovy, 
+						aspect_ratio, 
+						near_plane, 
+						far_plane, 
+						frame.depth_width(),
+						frame.depth_height());
+
 					const Eigen::Vector3f n(0, 0, 1);
 
 					vertices.push_back(vert_uv);
@@ -78,9 +86,9 @@ int main(int argc, char **argv)
 				}
 				else
 				{
-					vert_uv = window_coord_to_3d(Eigen::Vector2f(x, y), depth, fovy, aspect_ratio, near_plane, far_plane, depth_map_width, depth_map_height);
-					vert_u1v = window_coord_to_3d(Eigen::Vector2f(x + 1, y), depth_u1v, fovy, aspect_ratio, near_plane, far_plane, depth_map_width, depth_map_height);
-					vert_uv1 = window_coord_to_3d(Eigen::Vector2f(x, y + 1), depth_uv1, fovy, aspect_ratio, near_plane, far_plane, depth_map_width, depth_map_height);
+					vert_uv = window_coord_to_3d(Eigen::Vector2f(x, y), depth, fovy, aspect_ratio, near_plane, far_plane, frame.depth_width(), frame.depth_height());
+					vert_u1v = window_coord_to_3d(Eigen::Vector2f(x + 1, y), depth_u1v, fovy, aspect_ratio, near_plane, far_plane, frame.depth_width(), frame.depth_height());
+					vert_uv1 = window_coord_to_3d(Eigen::Vector2f(x, y + 1), depth_uv1, fovy, aspect_ratio, near_plane, far_plane, frame.depth_width(), frame.depth_height());
 
 					const Eigen::Vector3f n1 = vert_u1v - vert_uv;
 					const Eigen::Vector3f n2 = vert_uv1 - vert_uv;
