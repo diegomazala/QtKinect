087219ed1c6619666f26576a4144985d104de9f5
AVELL\Diego <diegomazala@gmail.com>
2016-09-02 09:36:42 -0300

    Function to generate normals from knt frme

--- src/Volumetric_helper.h
+++ src/Volumetric_helper.h
@@ -10,6 +10,7 @@
 #include "Projection.h"
 #include "ComputeRigidTransform.h"
 #include "Raycasting.h"
+#include "KinectFrame.h"
 
 
 #define DegToRad(angle_degrees) (angle_degrees * M_PI / 180.0)		// Converts degrees to radians.
@@ -545,6 +546,72 @@ static void update_volume(
 			params[index][1] = weight;
 
 			++index;
+		}
+	}
+}
+
+
+
+static void knt_frame_generate_normals(
+	const KinectFrame& frame, 
+	std::vector<Eigen::Vector3f>& vertices,
+	std::vector<Eigen::Vector3f>& normals,
+	std::vector<Eigen::Vector3f>& colors)
+{
+	vertices.clear();
+	normals.clear();
+	colors.clear();
+
+	const float fovy = 70.0f;
+	const float aspect_ratio = static_cast<float>(frame.depth_width()) / static_cast<float>(frame.depth_height());
+	const float near_plane = 0.1f;
+	const float far_plane = 10240.0f;
+
+	Eigen::Vector3f vert_uv, vert_u1v, vert_uv1;
+
+
+	for (int x = 0; x < frame.depth_width() - 1; ++x)
+	{
+		for (int y = 0; y < frame.depth_height() - 1; ++y)
+		{
+			const float depth = frame.depth[y * frame.depth_width() + x];
+			const float depth_u1v = frame.depth[y * frame.depth_width() + x + 1];
+			const float depth_uv1 = frame.depth[(y + 1) * frame.depth_width() + x];
+
+			if (depth < 0.01 || depth_u1v < 0.01 || depth_uv1 < 0.01)
+			{
+				continue;
+
+				vert_uv = window_coord_to_3d(
+					Eigen::Vector2f(x, y),
+					depth,
+					fovy,
+					aspect_ratio,
+					near_plane,
+					far_plane,
+					frame.depth_width(),
+					frame.depth_height());
+
+				const Eigen::Vector3f n(0, 0, 1);
+
+				vertices.push_back(vert_uv);
+				normals.push_back(n);
+				colors.push_back(n * 255.0f);
+			}
+			else
+			{
+				vert_uv = window_coord_to_3d(Eigen::Vector2f(x, y), depth, fovy, aspect_ratio, near_plane, far_plane, frame.depth_width(), frame.depth_height());
+				vert_u1v = window_coord_to_3d(Eigen::Vector2f(x + 1, y), depth_u1v, fovy, aspect_ratio, near_plane, far_plane, frame.depth_width(), frame.depth_height());
+				vert_uv1 = window_coord_to_3d(Eigen::Vector2f(x, y + 1), depth_uv1, fovy, aspect_ratio, near_plane, far_plane, frame.depth_width(), frame.depth_height());
+
+				const Eigen::Vector3f n1 = vert_u1v - vert_uv;
+				const Eigen::Vector3f n2 = vert_uv1 - vert_uv;
+				const Eigen::Vector3f n = n1.cross(n2).normalized();
+
+				vertices.push_back(vert_uv);
+				normals.push_back(n);
+				colors.push_back((n * 0.5f + Eigen::Vector3f(0.5, 0.5, 0.5)) * 255.0f);
+			}
 		}
 	}
 }
